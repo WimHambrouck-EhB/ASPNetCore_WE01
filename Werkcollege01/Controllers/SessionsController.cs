a85d8cb1474731ac54aa46a666837ebab63ffa15
Wim Hambrouck <wim.hambrouck@ehb.be>
2023-02-21 17:37:19 +0100

    Oefening 2: Sessies

---
+++ Werkcollege01/Controllers/SessionsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Werkcollege01.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public const string KEY_AANTAL_BEZOEKEN = "_AantalBezoeken";
+        public const string KEY_GEBRUIKER = "_Gebruiker";
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetInt32(KEY_AANTAL_BEZOEKEN) is not int teller)
+            {
+                teller = 0;
+            }
+
+            HttpContext.Session.SetInt32(KEY_AANTAL_BEZOEKEN, ++teller);
+
+            ViewData[KEY_AANTAL_BEZOEKEN] = teller;
+
+            return View();
+        }
+    }
+}
--- Werkcollege01/Program.cs
+++ Werkcollege01/Program.cs
@@ -3,6 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    // Hoe lang een sessie duurt (voor testing kan je een lagere waarde gebruiken) 
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+
+    // Als een cookie niet "essential" is, zal het pas werken nadat 
+    // de gebruiker toestemming gegeven heeft voor het bijhouden van gegevens 
+    // Opgelet: nooit gebruikersdata opslaan in essential cookies! (AVG/GDPR) 
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +33,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
