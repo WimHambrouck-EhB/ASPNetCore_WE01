17397b7f1a9192048d83d63f4402e41f1e19ac42
Wim Hambrouck <wim.hambrouck@ehb.be>
2023-02-21 17:28:38 +0100

    Oefening 1: form data

---
+++ Werkcollege01/Controllers/RegistreerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+
+namespace Werkcollege01.Controllers
+{
+    public class RegistreerController : Controller
+    {
+        private readonly ImmutableList<string> _mailClients =
+            ImmutableList.Create(
+                "Microsoft Outlook",
+                "Outlook Express",
+                "Thunderbird",
+                "Other…"
+            );
+
+        public IActionResult Index()
+        {
+            ViewBag.MailClients = _mailClients;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registreer(string? email, int mailclient, string? wachtwoord)
+        {
+            if (!string.IsNullOrWhiteSpace(email) && !string.IsNullOrWhiteSpace(wachtwoord))
+            {
+                ViewBag.Message = "Registratie succesvol!";
+            }
+
+            ViewBag.Email = email;
+            ViewBag.Client = _mailClients[mailclient];
+            ViewBag.MailClients = _mailClients;
+
+            return View("Index");
+        }
+    }
+}
