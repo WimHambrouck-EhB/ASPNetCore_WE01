596bde2e82d0049b7658251b68c2f4ba89490d30
Wim Hambrouck <wim.hambrouck@ehb.be>
2023-02-25 10:33:15 +0100

    GebruikersController aangemaakt

---
+++ Werkcollege01/Controllers/GebruikersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Werkcollege01.Controllers
+{
+    public class GebruikersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
