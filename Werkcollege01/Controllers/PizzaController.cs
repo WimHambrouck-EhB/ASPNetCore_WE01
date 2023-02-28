26393fd4d61d0ac9575dced7b68ccd02537488aa
Wim Hambrouck <wim.hambrouck@ehb.be>
2023-02-28 13:09:28 +0100

    Oef4: Pizza

---
+++ Werkcollege01/Controllers/PizzaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Werkcollege01.Models;
+
+namespace Werkcollege01.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            // geen Index view voor deze controller, gebruiker doorsturen naar /Pizza/Bestellen
+            return RedirectToAction(nameof(Bestellen));
+        }
+
+        public IActionResult Bestellen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Toon([Bind("Besteller,Soort,ExtraKaas,Betaalwijze")] Pizza? pizza)
+        {
+            if (pizza == null) {
+                return BadRequest("Invalid POST data");
+            }
+
+            List<string> errors = new();
+
+            if (string.IsNullOrWhiteSpace(pizza.Besteller))
+                errors.Add("Gelieve de naam van de besteller op te geven.");
+            if (string.IsNullOrWhiteSpace(pizza.Soort))
+                errors.Add("Gelieve de pizza die je wenst te bestellen op te geven.");
+            if (string.IsNullOrWhiteSpace(pizza.Betaalwijze))
+                errors.Add("Gelieve een betaalwijze te kiezen.");
+
+            ViewBag.Errors = errors;
+
+            return View(pizza);
+        }
+    }
+}
---
+++ Werkcollege01/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace Werkcollege01.Models
+{
+    public class Pizza
+    {
+        public string? Besteller { get; set; }
+        public string? Soort { get; set; }
+        public bool ExtraKaas { get; set; }
+        public string? Betaalwijze { get; set; }
+    }
+}
